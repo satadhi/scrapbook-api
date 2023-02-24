d01b024b08f52937d5e01feb5e8b1325bf2f478c
satadhi <haldersatadhi@gmail.com>
2023-02-25 01:24:00 +0530

    error fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "../models/user.js";
+import User from "../models/User.js";
 
 /* REGISTER USER*/
 export const register = async (req, res) => {
